ee338cdc309b69fc8b8e19f2f1233b72421e737e
Alice Won <alicewon4@gmail.com>
2020-01-24 11:46:16 -0800

    updated artgallery controller

--- app/controllers/artwork_galleries_controller.rb
+++ app/controllers/artwork_galleries_controller.rb
@@ -1,2 +1,27 @@
+require 'byebug'
+
 class ArtworkGalleriesController < ApplicationController
+  
+  def index
+    @artwork_galleries = ArtworkGallery.all
+    render :json => @artwork_galleries
+  end
+
+  def show
+    @artwork_gallery = ArtworkGallery.find(params[:id])
+  end
+
+  def new
+    @artwork_gallery = ArtworkGallery.new
+  end
+
+  def create
+    @artwork_gallery = ArtworkGallery.new(params.require(:artwork_gallery).permit(:artwork_id, :gallery_id))
+    byebug
+    if @artwork_gallery.save
+      render :json => @artwork_gallery
+    else
+      flash[:message] = "error"
+    end
+  end
 end
